be1a6834c76bda12e56cc172a3f0ad2d496c9f88
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-04-02 20:27:15 +0200

    solution for fraction doplnit chybu od učitele

--- PythonOOP/fraction.py
+++ PythonOOP/fraction.py
@@ -10,11 +10,18 @@ class Fraction:
     def __eq__(self, other): # porovnávám zlomek
         return self.nominator * other.denominator == self.denominator + other.nominator
 
-    def __add__(self, other):
+    def __add__(self, other): # plus
         common_denominator = self.denominator * other.denominator
         new_nominator = self.nominator * other.denominator + other.nominator * self.denominator
         return Fraction(new_nominator, common_denominator)
 
+    def __iadd__(self, other): #se znaménkem += a jednu proměnou rovnou přepisuji. Učitel to má ve svém kodu Fraction
+        if isinstance(other, Fraction):
+            common_denominator = self.denominator * other.denominator
+            self.nominator * other.denominator + other.nominator * self.denominator
+            self.denominator = common_denominator
+            return self
+        raise TypeError("Můj error: Unsupported operand type for +=: {} and {}")
 
 
 
--- PythonOOP/main.py
+++ PythonOOP/main.py
@@ -10,3 +10,9 @@ print(f1 == f2)
 
 f3 = f1 + f2
 print(f3)
+
+f3 += f2 # toto je __iadd__
+print(f3)
+
+f3 += 3 # toto je __iadd__
+print(f3)
