fcc3dd2d92f93179ab62c740d2b5ec0750ad43db
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-04-02 18:45:31 +0200

    first class vehicle

--- PythonOOP/vehicle.py
+++ PythonOOP/vehicle.py
@@ -4,6 +4,9 @@ class Vehicle:
     #inicializace při vytváření konkrétní třídy
         self.name = name
 
+    def start(self): #Metoda fce uvnitř nějaké třídy
+        print(f"vrvrvrvr startuju vozidlo {self.name}")
+
 
 v1 = Vehicle("Vehicle1") #list()
 v2 = Vehicle("Vehicle2")
@@ -13,3 +16,7 @@ v3 = Vehicle("Vehicle3")
 print(v1.name)
 print(v2.name)
 print(v3.name)
+print(type(v1))
+
+v2.start()
+
