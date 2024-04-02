4eb26d82811669a362c490bccfda2d3fe7ab2b2f
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-04-02 20:17:39 +0200

    Sčítání zlomků

--- PythonOOP/fraction.py
+++ PythonOOP/fraction.py
@@ -3,3 +3,19 @@ class Fraction:
     def __init__(self, nominator, denominator):
         self.nominator = nominator
         self.denominator = denominator
+
+    def __str__(self): #vypisuji zlomek
+        return (f"Zlomek: {self.nominator}/{self.denominator}")
+
+    def __eq__(self, other): # porovnávám zlomek
+        return self.nominator * other.denominator == self.denominator + other.nominator
+
+    def __add__(self, other):
+        common_denominator = self.denominator * other.denominator
+        new_nominator = self.nominator * other.denominator + other.nominator * self.denominator
+        return Fraction(new_nominator, common_denominator)
+
+
+
+
+
--- PythonOOP/main.py
+++ PythonOOP/main.py
@@ -2,3 +2,11 @@ from fraction import Fraction
 
 f1 = Fraction(1,3)
 
+print(f1)
+
+f2 = Fraction(1,3)
+
+print(f1 == f2)
+
+f3 = f1 + f2
+print(f3)
