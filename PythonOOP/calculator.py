ff9fbeef9ffe320f5d6e7832053a00c0dc9a0bc5
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-04-09 20:14:12 +0200

    rozšíření kalkulačky sdílenou věc přes celou třídu

--- PythonOOP/calculator.py
+++ PythonOOP/calculator.py
@@ -1,8 +1,20 @@
 class Calculator:
+    ans = 0
+
     @staticmethod
     def add(a, b):
         return a+b
     @staticmethod
     def divide(a, b):
         return a / b
+
+    @classmethod
+    def ans_add(cls, a):
+        cls.ans += a
+        return cls.ans
+
+    @classmethod
+    def clean_ans(cls):
+        cls.ans = 0
+
 
--- PythonOOP/main_calculator.py
+++ PythonOOP/main_calculator.py
@@ -1,5 +1,9 @@
 from calculator import *
 
+# sečtení z třídy calculate
 a = Calculator.add(3,6)
-
 print(a)
+
+b = Calculator.ans_add(8)
+c = Calculator.ans_add(4)
+print(c)
